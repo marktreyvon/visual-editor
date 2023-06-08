434be383e5911d3976c75a6c4c3ce9ba4b401ba7
zj_wbh <wbhong@zjaq.com>
2023-06-08 17:25:21 +0800

    fix：

--- src/editor/components/right-attribute-panel/components/LayerAttr.vue
+++ src/editor/components/right-attribute-panel/components/LayerAttr.vue
@@ -1,20 +1,48 @@
 <!-- 图层 -->
 <template>
-<div v-for='i in layerList' key='i.view.cell.id' @click='layerClick($event,i.view)'>
 
-  <img width='50' height='50' :src="pluginConfig.getComponent(i.view
-        .cell.shape)?.icon||'https://bpic.588ku.com/element_origin_min_pic/01/39/50/51573cc2a0b4083.jpg'" alt=''>
+<div style='height:700px;overflow-y: auto;overflow-x: hidden;padding: 0 6px'>
+  <el-row :gutter="20">
+    <el-col style='margin-top:6px' :span="24" v-for='(i,index) in layerList' key='i.view.cell.id' @click='layerClick($event,i.view)'>
+
+      <el-card :body-style="{padding: '0px 12px',height:'60px',display:'flex',justifyContent:'space-between',alignItems:'center' }">
+        <img width='50' height='50' :src="pluginConfig.getComponent(i.view
+        .cell.shape)?.icon||(i.view.cell.shape==='edge'?edgeLayerIcon:defaultIcon)" alt=''>
+        <div style="padding: 14px">
+          <el-switch
+              @change='showCell($event,index,i)'
+              v-model="layerList[index].isV"
+              class="ml-2"
+              inline-prompt
+              style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+              active-text="显示"
+              inactive-text="影藏"
+          />
+          <el-switch
+              v-model="layerList[index].isE"
+              @change='editCell($event,index,i)'
+              class="ml-2"
+              inline-prompt
+              style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+              active-text="可编辑"
+              inactive-text="禁用编辑"
+          />
+        </div>
+      </el-card>
+    </el-col>
+
+  </el-row>
 
 </div>
+
 </template>
 
 <script setup lang="ts">
 import {onMounted, ref, watch} from "vue";
 import {PluginConfig} from "@/editor/config";
-import { useEvents } from "../useEvents"
-
+import defaultIcon from "@/assets/defaultIcon.svg";
+import edgeLayerIcon from "@/assets/edgeLayerIcon.svg";
 const pluginConfig: IPluginConfig = PluginConfig.getInstance();
-
 const props = defineProps({
   cellList: {
     type: Array,
@@ -24,26 +52,29 @@ const props = defineProps({
     default: () => {}
   }
 })
-let {
-  initEvents, onChange ,
-} = useEvents();
+const showCell= (v: any, index: any, i: any)=>{
+
+   i.view.cell.setVisible(!v)
+
+}
+const editCell= (v: any, index: any, i: any)=>{
+
+
+}
 const layerList=ref<any>([])
 const layerClick=(e:any,d:any)=> {
 
   if(d.cell.shape!=='edge'){
-    console.log(d.cell.shape,"00000000000")
     d.graph.trigger('node:click',{ e:e, node: d.cell })
   }else{
     d.graph.clearTransformWidgets();
   }
   d.graph.trigger('cell:click', { e:e, cell: d.cell });
-  // d.graph.trigger('edge:click', {  edge: d.cell });
 }
-onMounted(()=>{
-  initEvents()
-})
+
 watch(props,(v)=>{
   layerList.value=v.cellList
+
 })
 
 </script>
