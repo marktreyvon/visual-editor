90f4425bda78e01014b896a8c1083d47d801789d
zj_wbh <wbhong@zjaq.com>
2023-06-20 11:39:01 +0800

    fix：去掉修改属性的撤销

--- src/editor/config/CanvasConfig.ts
+++ src/editor/config/CanvasConfig.ts
@@ -184,6 +184,7 @@ class CanvasConfig implements ICanvasConfig {
             this.graph.use(
                 new History({
                     enabled: true,
+                    ignoreChange: true,
                 })
             );
         }
