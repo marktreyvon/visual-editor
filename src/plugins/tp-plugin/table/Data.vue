87171cace94f071e9ea035cd55e6ea0e9ddb83bb
Fizz Pang <pangmengchen@gmail.com>
2023-07-17 20:35:55 +0800

    feat: 设备表格

--- src/plugins/tp-plugin/table/Data.vue
+++ src/plugins/tp-plugin/table/Data.vue
@@ -39,6 +39,7 @@ export default {
       bindOptions: [
         { value: 'static', label: '静态数据' }, 
         { value: 'dynamic', label: '动态数据'},
+        { value: 'device', label: '设备数据'},
       ]
     }
   },
